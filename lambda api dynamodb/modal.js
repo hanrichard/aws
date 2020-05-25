ac223380bae3df650ff6195cbb4cdc0f8040776e
richardhan <hxy.314@gmail.com>
2020-05-25 21:40:21 +1000

    modal and post

---
+++ lambda api dynamodb/modal.js
@@ -0,0 +1,16 @@
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "User",
+    "type": "object",
+    "properties": {
+        "id": {
+            "type": "string"
+        },
+        "firstname": {
+            "type": "string"
+        },
+        "lastname": {
+            "type": "string"
+        }
+    }
+}
