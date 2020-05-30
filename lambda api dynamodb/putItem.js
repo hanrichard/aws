44fda2f35100672c00079328e002459fe7c0af6d
richardhan <hxy.314@gmail.com>
2020-05-30 15:48:47 +1000

    updatge

--- lambda api dynamodb/putItem.js
+++ lambda api dynamodb/putItem.js
@@ -6,27 +6,24 @@ AWS.config.update({ region: "ap-southeast-2"});
 exports.handler = async (event, context) => {
   const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08"});
   const documentClient = new AWS.DynamoDB.DocumentClient({ region: "ap-southeast-2"});
-
+  
   let responseBody = "";
-  let statusCode = 0;
-
-  const { id, firstname, lastname } = JSON.parse(event.body);
-
-  const params = {
+  let statusCode= 0;
+  
+  const { id } = event.pathParameters;
+const params = {
     TableName: "Users",
-    Item: {
-      id: id,
-      firstname: firstname,
-      lastname: lastname
+    Key: {
+      id: id
     }
   }
 
   try {
-    const data = await documentClient.put(params).promise();
-    responseBody = JSON.stringify(data);
-    statusCode = 201;
+    const data = await documentClient.get(params).promise();
+    responseBody = JSON.stringify(data.Item);
+    statusCode = 200;
   } catch (err) {
-    responseBody = `Unable to put user data`;
+    responseBody = `Unable to get user data`;
     statusCode = 403;
   }
 
